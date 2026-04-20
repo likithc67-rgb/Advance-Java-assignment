b9d26e03a842e8adabf2a8d311a4b10123f3ec33
likithc67-rgb <likithc67@gmail.com>
2026-04-20 23:33:16 +0530

    Add files via upload

---
+++ lab4/PerformanceTest.java
@@ -0,0 +1,30 @@
+package lab4;
+
+
+//Java Program to demonstrate the performance of StringBuffer and StringBuilder
+public class PerformanceTest {
+
+ public static void main(String[] args) {
+
+     long startTime = System.currentTimeMillis();
+
+     // StringBuffer test
+     StringBuffer sb = new StringBuffer("Java");
+     for (int i = 0; i < 10000; i++) {
+         sb.append("Tpoint");
+     }
+     System.out.println("Time taken by StringBuffer: "
+             + (System.currentTimeMillis() - startTime) + " ms");
+
+     // StringBuilder test
+     startTime = System.currentTimeMillis();
+     StringBuilder sb2 = new StringBuilder("Java");
+     for (int i = 0; i < 10000; i++) {
+         sb2.append("Tpoint");
+     }
+     System.out.println("Time taken by StringBuilder: "
+             + (System.currentTimeMillis() - startTime) + " ms");
+ }
+}
+
+
